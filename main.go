d4bb3c8176e6e5d994bbba0b29999c69629c5c04
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-10-10 19:17:45 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,51 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"math/rand"
-	"os"
-	"reflect"
-	"time"
+	"fmt"
 )
 
 var (
-	charactersList      = 1234567890
-	cardCharactersCount = 16
-	expCharactersCount  = 4
-	csvCharactersCount  = 4
-	outputFile          = "output.json"
+	//
 )
 
-type MembershipReport struct {
-	// NUMBER string `json:"card-number"`
-	EXP    string `json:"exp-date"`
-	CVC    string `json:"cvc"`
-}
-
-func RandomString() int {
-	// use a random seed
-	rand.Seed(time.Now().UnixNano())
-	// generate all the random strings here.
-	randInt := rand.Intn(10)
-	// return value
-	return randInt
-}
-
-func file() {
-	file, err := os.Create(outputFile)
-	if err != nil {
-		log.Println(err)
-	}
-	if _, err = file.Write(b); err != nil {
-		fmt.Printf("Error writing to a file %s", err)
-	}
-	file.Close()
-}
+const (
+	//
+)
 
 func main() {
-	for {
-		// loop everything here.
-	    }
+	//
 }
