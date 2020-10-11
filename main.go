42daf4ba76bf4cebc1587a6a58eaf4fd4851f086
Prajwal Koirala <prajwalkoirala23@protonmail.com>
2020-10-11 15:25:51 -0400

    updated the code

--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"log"
 	"io/ioutil"
+	"encoding/json"
+	"time"
 )
 
 var (
-	//
+	charactersList  = 1234567890
+	charactersCount = 16
 )
 
 const (
-	//
+	outputFile = "output.json"
 )
+
+type MembershipReport struct {
+	EXP    string `json:"exp-date"`
+	CVC    string `json:"cvc"`
+}
 
 func main() {
 	//
